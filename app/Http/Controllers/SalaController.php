1ceeedd43f7e3e30d33881cc5e54d4d640396ded
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-15 17:22:08 +0100

    Functions (Create & show sala) from SalaController

--- app/Http/Controllers/SalaController.php
+++ app/Http/Controllers/SalaController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Sala;
 
 class SalaController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function salaWith(User $user){
+        $user_auth = auth()->user();
+        $user_b = $user;
+
+        $sala = $user_auth->salas()->wherehas('users', function($query) use ($user_b){
+            $query->where('sala_user.user_id', $user_b->id);
+        })->first();
+
+        if(!$sala){
+            //Creamos nueva sala
+            $sala = Sala::create([]);
+            $sala->users()->sync([$user_auth->id, $user_b->id]);
+        }
+
+        return redirect()->route('sala.show', $sala);
+    }
+
+    public function show(Sala $sala){
+        return view('sala', [
+            'sala' => $sala
+        ]);
+    }
 }
