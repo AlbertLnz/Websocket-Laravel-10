df836b77a5c4e06c58928a9af5e3cb80dab7eaf2
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-15 17:35:50 +0100

    Broadcast channel created on 'channels.php'

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('sala.{sala_id}', function($user, $sala_id){
+    if($user->salas->contains($sala_id)){
+        return $user;
+    }
+});
